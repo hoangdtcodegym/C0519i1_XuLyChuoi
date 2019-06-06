99d8968ae68573bdd6f0698fa375aa16cf079334
monkey2095 <50616245+monkey2095@users.noreply.github.com>
2019-06-06 23:35:09 +0700

    Delete checkname.js

--- checkname.js
+++
@@ -1,8 +0,0 @@
-function checknameclass(str) {
-    nameclass = /^[ACP][0-9]{4,4}[GHIKLM]$/;
-    if (nameclass.test(str)){
-        alert("Chuẩn mẹ luôn");
-    } else {
-        alert("Ngu lồn");
-    }
-}
