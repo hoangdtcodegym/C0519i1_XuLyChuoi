0671579a3b80427f19e8a2ce1630bead780a96b1
monkey2095 <50616245+monkey2095@users.noreply.github.com>
2019-06-06 23:35:19 +0700

    Delete checknumber.js

--- checknumber.js
+++
@@ -1,8 +0,0 @@
-function checkphone(str) {
-    phonenumber = /^[0-9][0-9][\-][0-9]{10,11}$/;
-    if (phonenumber.test(str)){
-        alert("Chuẩn mẹ luôn");
-    } else {
-        alert("Ngu lồn");
-    }
-}
