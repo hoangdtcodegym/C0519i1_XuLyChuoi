3e03bd71612c2917f4c601bc5a71b58c6c58db16
DuyTan <duytan@Acer.V5-471>
2019-06-07 08:03:43 +0700

    C0519i1_Duy Tan

---
+++ isUpperCase.js
@@ -0,0 +1,8 @@
+function isFirstLetterUpperCase(str) {
+
+    if (regexp.test(str)) {
+        console.log("String is first character is uppercase");
+    } else {
+        console.log("String is first character is not uppercase");
+    }
+}
